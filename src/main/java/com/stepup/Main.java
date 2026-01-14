f090d020d46f864de71cbb2761b33b84b122fdaa
Alexey Fedorenko <stud.virus@gmail.com>
2026-01-14 16:39:48 +0300

    Edit Main - add arifmetic calculations

--- src/main/java/com/stepup/Main.java
+++ src/main/java/com/stepup/Main.java
@@ -4,8 +4,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите число:");
+        int number = new Scanner(System.in).nextInt();
+
+        System.out.println("Введите число 2:");
+        int number2 = new Scanner(System.in).nextInt();
+
+        int raznost = number - number2;
+        System.out.println(raznost);
+
+        int sum = number2 + number;
+        System.out.println(sum);
+
+        int multiplication = number * number2;
+        System.out.println(multiplication);
+
+        double quotient = (double) number / number2;
+        System.out.println(quotient);
+
     }
 }
